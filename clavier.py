f93659a80492c0fd983437717fecec33a5088514
maximepesche <60397547+maximepesche@users.noreply.github.com>
2020-02-04 15:10:01 +0100

    Add files via upload

--- clavier.py
+++ clavier.py
@@ -1,85 +1,81 @@
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
-from time import sleep
-
-
-class Clavier:
-
-	def __init__(self, fs, volume, T_segment, T_cross):
-		"""
-		fs : fréquence de répétition
-		volume : amplitude des sinusoïde. doit pas depasser 1
-		T_segment/N_sement : durée/taille des array de son
-		T_cross : durée/taille de la section reduite
-		pour enlever les bruits au début et à la fin		
-
-		frequence : dict avec avec les fréquences associer aux nom des notes
-		note : dict avec des bool pour chaque notes
-			si True, la note est joué
-			si False, la note est pas joué
-
-		"""
-		self.fs = fs
-		self.volume = volume
-
-		N_segment = int(T_segment*fs)		
-		self.T_segment = T_segment
-		self.N_segment = N_segment
-		self.X_segment = np.linspace(0,T_segment,N_segment)
-
-		N_cross = int(T_cross*fs)
-		self.N_cross = N_cross
-		self.A_cross = np.linspace(0,1,N_cross)
-
-		note_nom = ["Do","Do#","Re","Re#","Mi","Mi#","Fa","Fa#","Sol","Sol#","La","La#","Si"]
-		note_frequence = [261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440.0, 466.164, 493.883]
-
-		frequence = {}
-		note = {}
-		for i in range(len(note_nom)-1):
-			frequence[note_nom[i]] = note_frequence[i]
-			note[note_nom[i]] = False
-		self.frequence = frequence
-		self.note = note
-
-
-	def start(self, note):
-		#joue la note en question
-		self.note[note] = True
-
-
-	def stop(self,note):
-		#arrete de jouer la note en question
-		self.note[note] = False
-
-
-	def create_son(self):
-		#creer le tableau de son
-
-		Y = np.zeros(self.N_segment)
-
-		for key in self.note.keys():
-			if self.note[key] == True:
-				Y+= self.volume*np.sin(2*np.pi*self.frequence[key]*self.X_segment)
-
-		Y[0 : self.N_cross]*= self.A_cross
-		Y[self.N_segment-self.N_cross : self.N_segment]*= self.A_cross[::-1]
-		#Y[self.N_segment-self.N_cross : self.N_segment]= 0
-
-		return Y
-
 
 fs = 44100
-volume = 0.5
-T_segment = 1
-T_cross = 0.005
+blocksize = 1000
+freq = 440
+duration = 15
+start_idx = 0
+N_cross = 100
+coeff_exp = 1
 
-clavier = Clavier(fs,volume,T_segment,T_cross)
-clavier.start("Re")
 
-note = clavier.create_son()
+class Tone:
+	
+	def __init__(self):
+		self.list_tone = []
 
-sd.play(note,fs)
-sd.wait()
+		nom_note = ("Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si")
+		freq_note = (261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883)
+		notes = {}
+		for i in range(len(nom_note)):
+			notes[nom_note[i]] = freq_note[i]
+		self.notes = notes
+
+	def start_tone(self,note,N):
+		self.list_tone.append( [note, N, True])
+
+	def stop_tone(self,note,N):
+		for i,tone in enumerate(self.list_tone):
+			if tone[0] == note:
+				self.list_tone[i][2] = False
+				self.list_tone[i][1] = N
+			
+
+
+def generer_son(Tone, N_time, frames, N_cross, fs):
+	outdata = np.zeros(frames)
+
+	t = (N_time + np.arange(frames)) / fs
+
+	for tone in Tone.list_tone:
+
+		freq = Tone.notes[tone[0]]
+		N_note = tone[1]
+		note  = np.sin(2*np.pi*t*freq)	
+
+		if tone[2] == True:
+			note*= np.exp(-coeff_exp * (t-N_note/fs))
+		outdata+= note
+	outdata = outdata.reshape(-1,1)
+	return outdata
+
+
+
+def callback(indata, outdata, frames, time,  status):
+	if status:
+		print(status)
+	global start_idx
+	global tone
+
+	note = generer_son(tone, start_idx, frames, N_cross, fs)
+
+	outdata[:] = note
+
+	start_idx+= frames
+
+
+tone = Tone()
+
+tone.start_tone("Fa",44100)
+
+
+with sd.Stream(channels=2, callback=callback, blocksize=blocksize):
+	sd.sleep(int(duration * 1000))
+
+
+print(tone.list_tone)
+
+
 
